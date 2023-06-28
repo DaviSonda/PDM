0114981f13ed0b184fb941a96a3f8f57611a5bba
Davi Sonda Sonego <davisonego16@gmail.com>
2023-06-28 08:21:20 -0300

    mock

--- PetShop/app/src/main/java/com/davisonego/petshop/Globals.java
+++ PetShop/app/src/main/java/com/davisonego/petshop/Globals.java
@@ -1,5 +1,5 @@
 package com.davisonego.petshop;
 
 public class Globals {
-    public static final String url = "http://192.168.1.13:3000";
+    public static final String url = "http://192.168.128.191:3000";
 }
